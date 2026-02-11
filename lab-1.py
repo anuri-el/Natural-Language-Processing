26567e013df1402b93b9f201259582a7118e6cb7
anuri-el <sofia.poteychuk@gmail.com>
2026-02-11 23:16:26 +0200

    add text filtering

--- lab-1.py
+++ lab-1.py
@@ -8,9 +8,13 @@ def main():
     site_int, site_url = get_site(site_urls)
 
     if site_int == 1:
-        parser_pravda(site_url)
+        news_txt = parser_pravda(site_url)
     elif site_int == 2:
-        parser_zaxid(site_url)
+        news_txt = parser_zaxid(site_url)
+    else:
+        print("no")
+
+    text_filter(news_txt)
 
 
 def get_site(site_urls):
@@ -44,11 +48,14 @@ def parser_pravda(site_url):
 
     article_tags = soup.find_all("div", class_="article_title")
     
-    for article_tag in article_tags:
-        articale_title = article_tag.text
-        print(articale_title.strip())
+    news_txt = "news_pravda_raw.txt"
+    with open(news_txt, "w", encoding="utf-8") as output_file:
+        for article_tag in article_tags:
+            article_title = article_tag.text.strip()
+            output_file.write(f"{article_title}\n")
     
-    print()
+    return news_txt
+
 
 def parser_zaxid(site_url):
     response = requests.get(site_url)
@@ -59,10 +66,36 @@ def parser_zaxid(site_url):
 
     soup = BeautifulSoup(site_html, "html.parser")
     news_tags = soup.find_all("div", class_="news-title")
-    for news_tag in news_tags:
-        news_title = news_tag.text
-        print(news_title.strip())
 
+    news_txt = "news_zaxid_raw.txt"
+    with open(news_txt, "w", encoding="utf-8") as output_file:
+        for news_tag in news_tags:
+            news_title = news_tag.text.strip()
+            output_file.write(f"{news_title}\n")
+
+    return news_txt
+
+
+def text_filter(filename):
+    with open(filename, encoding="utf-8") as file:
+        text = file.read()
+
+    chars_to_replace = ["\n", ",", ".", "!", "?", ":", ";", "\"", "–", "-"]
+    for ch in chars_to_replace:
+        text = text.replace(ch, " ")
+
+    text = text.lower()
+    words = text.split()
+    words.sort()
+    words_dict = dict()
+    for word in words:
+        if word in words_dict:
+            words_dict[word] = words_dict[word] + 1
+        else:
+            words_dict[word] = 1
+    
+    print(f"words: {len(words)}")
+    print(f"unique words: {len(words_dict)}")
 
 
 if __name__ == "__main__":
