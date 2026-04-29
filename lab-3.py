a320bf9e60af64bdf04d367d1efc2c527636df25
anuri-el <sofia.poteychuk@gmail.com>
2026-04-30 00:23:27 +0300

    add vectorizers

--- lab-3.py
+++ lab-3.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 
 
 OUTPUT_DIR = "outputs"
@@ -19,6 +20,8 @@ def main():
         topic_descriptions = df[df["topic"] == topic]["description"].tolist()
         TOPIC_DOCS[topic] = " ".join(topic_descriptions)
 
+    tfidf_vec, tfidf_matrix, bow_vec, bow_matrix = build_vectorizers(TOPIC_DOCS)
+
     # while True:
     #     try:
     #         text = input("\n > ").strip()
@@ -29,8 +32,16 @@ def main():
     #         break
 
 
+def build_vectorizers(topic_docs):
+    corpus = list(topic_docs.values())
 
+    tfidf_vec = TfidfVectorizer(analyzer="word", token_pattern=r"(?u)\b\w+\b", ngram_range=(1,2), min_df=1, sublinear_tf=True)
+    tfidf_matrix = tfidf_vec.fit_transform(corpus)
 
+    bow_vec = CountVectorizer(analyzer="word", token_pattern=r"(?u)\b\w+\b", ngram_range=(1,1), min_df=1)
+    bow_matrix = bow_vec.fit_transform(corpus)
+
+    return tfidf_vec, tfidf_matrix, bow_vec, bow_matrix
 
 
 if __name__ == "__main__":
